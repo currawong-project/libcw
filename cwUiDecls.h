c4b65688339cc08d34ccc19f295cdd520462edce
kevin <kevin@larke.org>
2022-05-21 09:28:17 -0400

    uiDecls.h : Added explicit numbers to enum's comments.

--- cwUiDecls.h
+++ cwUiDecls.h
@@ -13,27 +13,27 @@ namespace cw
     
     typedef enum
     {
-     kInvalidOpId,
-     kConnectOpId,   // A new remote user interface was connected
-     kInitOpId,      // A remote user interface instance was created and is available. It needs to be updated with the current state of the UI from the server.
-     kValueOpId,     // The value of a remote user interface control changed. Send this value to the application engine.
-     kCorruptOpId,   // The value of the remote user interface is invalid or corrupt.
-     kClickOpId,     // A element on a remote user interface was clicked.
-     kSelectOpId,    // An element on a remote user interface was is 'selected' or 'deselected'.
-     kEchoOpId,      // A remote user interface is requesting an application engine value. The the current value of a ui element must be sent to the remote UI.
-     kIdleOpId,      // The application (UI server) is idle and waiting for the next event from a remote UI.
-     kDisconnectOpId // A reemot user interface was disconnected. 
+      kInvalidOpId,   // 0
+      kConnectOpId,   // 1 A new remote user interface was connected
+      kInitOpId,      // 2 A remote user interface instance was created and is available. It needs to be updated with the current state of the UI from the server.
+      kValueOpId,     // 3 The value of a remote user interface control changed. Send this value to the application engine.
+      kCorruptOpId,   // 4 The value of the remote user interface is invalid or corrupt.
+      kClickOpId,     // 5 A element on a remote user interface was clicked.
+      kSelectOpId,    // 6 An element on a remote user interface was is 'selected' or 'deselected'.
+      kEchoOpId,      // 7 A remote user interface is requesting an application engine value. The the current value of a ui element must be sent to the remote UI.
+      kIdleOpId,      // 8 The application (UI server) is idle and waiting for the next event from a remote UI.
+      kDisconnectOpId // 9 A remote user interface was disconnected. 
     } opId_t;
 
     typedef enum
     {
-     kInvalidTId,
-     kBoolTId,
-     kIntTId,
-     kUIntTId,
-     kFloatTId,
-     kDoubleTId,
-     kStringTId
+      kInvalidTId,  // 0
+      kBoolTId,     // 1
+      kIntTId,      // 2
+      kUIntTId,     // 3 
+      kFloatTId,    // 4
+      kDoubleTId,   // 5
+      kStringTId    // 6
     } dtypeId_t;
 
 
