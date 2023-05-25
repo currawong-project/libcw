aefa0eb1d8430e40a34a92a9c5a43edc19cb3191
kevin <kevin@larke.org>
2023-05-25 16:05:19 -0400

    cwSvgScoreFollow.h : Moved ssf_note_on_t to cwScoreFollowerPerf.h.

--- cwSvgScoreFollow.h
+++ cwSvgScoreFollow.h
@@ -2,14 +2,6 @@ namespace cw
 {
   namespace score_follower
   {
-    typedef struct ssf_note_on_str
-    {
-      double  sec;
-      unsigned muid;
-      uint8_t pitch;
-      uint8_t vel;
-      uint8_t pad[2];
-    } ssf_note_on_t;
 
     rc_t svgScoreFollowWrite( cmScH_t         cmScH,
                               cmScMatcher*    matcher,
