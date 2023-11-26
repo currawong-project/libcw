8fd00ac353c54367acd3cf6c7c79540614566bbc
kevin <kevin@larke.org>
2023-11-26 15:27:59 -0500

    cwSfAnalysis.h/cpp, Makefile.am : Initial commit.

---
+++ cwSfAnalysis.h
@@ -0,0 +1,16 @@
+namespace cw
+{
+  namespace sf_analysis
+  {
+
+    rc_t gen_analysis( sfscore::handle_t        scH,
+                       const sftrack::result_t* resultA,
+                       unsigned                 resultN,
+                       unsigned                 begLoc,
+                       unsigned                 endLoc,
+                       const char*              fname );
+
+    
+
+  }
+}
