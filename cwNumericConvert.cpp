47466a487392c6409cbc15f947e3c4d7e829d16c
kevin <kevin@larke.org>
2024-11-29 13:15:02 -0500

    cwNumericConvert.cpp, Makefile.am : Initial commit.

---
+++ cwNumericConvert.cpp
@@ -0,0 +1,30 @@
+#include "cwCommon.h"
+#include "cwLog.h"
+#include "cwCommonImpl.h"
+#include "cwMem.h"
+#include "cwFile.h"
+#include "cwTest.h"
+#include "cwLex.h"
+#include "cwText.h"
+#include "cwTest.h"
+#include "cwNumericConvert.h"
+
+
+cw::rc_t cw::numericConvertTest( const test::test_args_t& args )
+{
+  int8_t x0 = 3;
+  int x1 = 127;
+  
+  cw::numeric_convert( x1, x0 );
+  cwLogPrint("%i %i\n",x0,x1);
+    
+
+  int v0 = -1;
+  double v1 = -1;
+  cw::string_to_number("123",v0);
+  cw::string_to_number("3.4",v1);
+  cwLogPrint("%i %f\n",v0,v1 );
+
+  return cw::kOkRC;
+}
+
