c292c009e93da263e35ee271bfa9d543b8a46fed
kevin <kevin@larke.org>
2025-04-21 09:31:05 -0400

    cwScoreFollow2.h/cpp,cwScoreFollow2Test.cpp : on_new_note() now returns score velocity.

--- cwScoreFollow2.h
+++ cwScoreFollow2.h
@@ -6,22 +6,20 @@ namespace cw
     typedef handle< struct sf_str > handle_t;
 
     typedef struct args_str
-    {
-      perf_score::handle_t scoreH;
-      
+    {      
       double pre_affinity_sec;  // 1.0 look back affinity duration
       double post_affinity_sec; // 3.0 look forward affinity duration
       double pre_wnd_sec;       // 2.0 look back search window 
       double post_wnd_sec;      // 5.0 look forward search window
         
-      double decay_coeff;          // 0.995affinity decay coeff
+      double decay_coeff;          // 0.995 affinity decay coeff
 
       double d_sec_err_thresh_lo;  // 0.4 reject if d_loc > d_loc_thresh_lod and d_time > d_time_thresh_lo
-      int d_loc_thresh_lo;    // 3  
+      int    d_loc_thresh_lo;      //   3  
       
       double d_sec_err_thresh_hi;  // 1.5 reject if d_loc != 0 and d_time > d_time_thresh_hi
-      int      d_loc_thresh_hi;    // 4 reject if d_loc > d_loc_thresh_hi
-      int      d_loc_stats_thresh; // 3 reject for time stats updates if d_loc > d_loc_stats_thresh
+      int      d_loc_thresh_hi;    // 4   reject if d_loc > d_loc_thresh_hi
+      int      d_loc_stats_thresh; // 3   reject for time stats updates if d_loc > d_loc_stats_thresh
 
       bool rpt_fl;  // set to turn on debug reporting
       
@@ -29,13 +27,13 @@ namespace cw
 
     rc_t parse_args( const object_t* cfg, args_t& args );
 
-    rc_t create( handle_t& hRef, const args_t& args );
+    rc_t create( handle_t& hRef, const args_t& args, perf_score::handle_t scoreH );
 
     rc_t destroy( handle_t& hRef );
 
     rc_t reset( handle_t h, unsigned beg_loc_id, unsigned end_loc_id );
 
-    rc_t on_new_note( handle_t h, unsigned uid, double sec, uint8_t pitch, uint8_t vel, unsigned& loc_id );
+    rc_t on_new_note( handle_t h, unsigned uid, double sec, uint8_t pitch, uint8_t vel, unsigned& loc_id_ref, unsigned& score_vel_ref );
 
     // Decay the affinity window and if necessary trigger a cycle of async background processing
     rc_t do_exec( handle_t h );
