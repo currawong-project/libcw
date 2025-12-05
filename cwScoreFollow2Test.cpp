9d1bc6fea86e01209bc710df29034a36987d268f
kevin <kevin@larke.org>
2025-12-05 11:17:07 -0500

    cwScoreFollowTest2.cpp : Restructured to allow an alternative CSV file format and optionally print per-note results to log.

--- cwScoreFollow2Test.cpp
+++ cwScoreFollow2Test.cpp
@@ -27,6 +27,11 @@ namespace cw
 {
   namespace score_follow_2
   {
+    enum {
+      kCsvVersion0Id,
+      kCsvVersion1Id,
+    };
+    
     typedef struct midi_evt_str
     {
       unsigned uid;
@@ -43,12 +48,20 @@ namespace cw
       unsigned    sampleN;
     } midi_file_t;
 
-    rc_t parse_midi_file( const char* midi_csv_fname, double srate, midi_file_t& mf )
+    rc_t parse_midi_file( const char* midi_csv_fname,
+                          double srate,
+                          unsigned beg_row_idx,
+                          unsigned end_row_idx,
+                          unsigned filter_dev_idx,
+                          unsigned version_id,
+                          midi_file_t& mf )
     {
       rc_t          rc       = kOkRC;
-      csv::handle_t csvH;      
-      const char*   titleA[] = { "UID","trk","amicro","type","ch","D0","D1","sci_pitch" };
-      unsigned      titleN   = sizeof(titleA)/sizeof(titleA[0]);
+      csv::handle_t csvH;
+      const char*   v0_titleA[] = { "UID","trk","amicro","type","D0","D1","sci_pitch" };
+      const char*   v1_titleA[] = { "sec","status","d0","d1" };
+      const char**  titleA      = version_id==kCsvVersion0Id ? v0_titleA : v1_titleA;
+      unsigned      titleN      = version_id==kCsvVersion0Id ? sizeof(v0_titleA)/sizeof(v0_titleA[0]) : sizeof(v1_titleA)/sizeof(v1_titleA[0]);
       unsigned      line_idx = 0;
       unsigned      lineN    = 0;
       
@@ -56,6 +69,15 @@ namespace cw
       {
         rc = cwLogError(rc,"MIDI CSV file open failed.");
         goto errLabel;
+      }
+
+      if( filter_dev_idx != kInvalidIdx )
+      {
+        if( csv::title_col_index(csvH,"dev") == kInvalidIdx )
+        {
+          rc = cwLogError(kInvalidArgRC,"The performance CSV file '%s' does not have a 'dev' column.",cwStringNullGuard(midi_csv_fname));
+          goto errLabel;
+        }
       }
 
       if((rc = line_count(csvH,lineN)) != kOkRC )
@@ -72,14 +94,154 @@ namespace cw
         unsigned uid;
         unsigned amicro;
         const char* type = nullptr;
+        unsigned status;
+        unsigned d0;
+        unsigned d1;
+        unsigned dev_idx;
+        double sec;
+
+        // if a begin/end row range was given and this row is outside that range
+        if( beg_row_idx != kInvalidIdx && (beg_row_idx > line_idx || line_idx > end_row_idx) )
+          continue;
+
+        if( version_id == 0)
+        {
+          if((rc = getv(csvH,"UID",uid,"amicro",amicro,"type",type,"D0",d0,"D1",d1)) != kOkRC )
+          {
+            cwLogError(rc,"Error reading CSV line number %i.",line_idx+1);
+            goto errLabel;
+          }
+
+          sec = amicro/1000000.0;
+        }
+        else
+        {
+          if((rc = getv(csvH,"sec",sec,"status",status,"d0",d0,"d1",d1)) != kOkRC )
+          {
+            cwLogError(rc,"Error reading CSV line number %i.",line_idx+1);
+            goto errLabel;
+          }
+
+          uid = line_idx;
+          status &= 0xf0; // remove channel
+          
+        }
+
+        if( filter_dev_idx != kInvalidIdx )
+        {
+          if((rc = getv(csvH,"dev",dev_idx)) != kOkRC )
+          {
+            cwLogError(rc,"Error reading CSV 'dev' column on line number:%i.",line_idx+1);
+            goto errLabel;
+          }
+
+          // if a dev_idx filter was given and the device does not match 
+          if( dev_idx != filter_dev_idx )
+            continue;
+        }
+
+        
+        bool is_note_on_fl = version_id==0 ? textIsEqual(type,"non") : status==144;
+        
+        if(is_note_on_fl)
+        {
+          midi_evt_t& m = mf.evtA[ mf.evtN++ ];
+
+          assert( d0 <= 127 && d1 <= 127 );
+          
+          m.pitch      = (uint8_t)d0;
+          m.vel        = (uint8_t)d1;
+          m.sec        = sec;
+          m.sample_idx = (unsigned)(m.sec * srate);
+          m.uid        = uid;
+          
+          mf.sampleN = m.sample_idx;
+        }
+      }
+      
+    errLabel:
+      destroy(csvH);
+      
+      if( rc == kEofRC )
+        rc = kOkRC;
+
+      if( rc != kOkRC )
+        rc = cwLogError(rc,"MIDI csv file parse failed on '%s'.",cwStringNullGuard(midi_csv_fname));
+      return rc;      
+    }
+    
+    
+    rc_t parse_midi_file1( const char* midi_csv_fname, double srate, unsigned beg_row_idx, unsigned end_row_idx, unsigned filter_dev_idx, midi_file_t& mf )
+    {
+      rc_t          rc       = kOkRC;
+      csv::handle_t csvH;      
+      const char*   titleA[] = { "UID","trk","amicro","type","ch","D0","D1","sci_pitch" };
+      unsigned      titleN   = sizeof(titleA)/sizeof(titleA[0]);
+      unsigned      line_idx = 0;
+      unsigned      lineN    = 0;
+      
+      if((rc = csv::create(csvH,midi_csv_fname,titleA,titleN)) != kOkRC )
+      {
+        rc = cwLogError(rc,"MIDI CSV file open failed.");
+        goto errLabel;
+      }
+
+      if( filter_dev_idx != kInvalidIdx )
+      {
+        if( csv::title_col_index(csvH,"dev") == kInvalidIdx )
+        {
+          rc = cwLogError(kInvalidArgRC,"The performance CSV file '%s' does not have a 'dev' column.",cwStringNullGuard(midi_csv_fname));
+          goto errLabel;
+        }
+      }
+
+      if((rc = line_count(csvH,lineN)) != kOkRC )
+      {
+        rc = cwLogError(rc,"MIDI CSV line count access failed.");
+        goto errLabel;
+      }
+
+      mf.evtA = mem::allocZ<midi_evt_t>(lineN);
+      mf.evtN = 0;
+
+      // if default row filter values were given then set them the first/last line of the file
+      if( beg_row_idx == kInvalidIdx )
+        beg_row_idx = 0;
+      
+      if( end_row_idx == kInvalidIdx )
+        end_row_idx = lineN;
+  
+      for(; (rc = next_line(csvH)) == kOkRC; ++line_idx )
+      {
+        unsigned uid;
+        unsigned amicro;
+        const char* type = nullptr;
         unsigned ch;
         unsigned d0;
         unsigned d1;
+        unsigned dev_idx;
+
+        // if a begin/end row range was given and this row is outside that range
+        if( beg_row_idx > line_idx || line_idx > end_row_idx) 
+          continue;
         
         if((rc = getv(csvH,"UID",uid,"amicro",amicro,"type",type,"ch",ch,"D0",d0,"D1",d1)) != kOkRC )
         {
-          cwLogError(rc,"Error reading CSV line %i.",line_idx+1);
+          cwLogError(rc,"Error reading CSV line number %i.",line_idx+1);
           goto errLabel;
+        }
+
+        if( filter_dev_idx != kInvalidIdx )
+        {
+          if((rc = getv(csvH,"dev",dev_idx)) != kOkRC )
+          {
+            cwLogError(rc,"Error reading CSV 'dev' column on line number:%i.",line_idx+1);
+            goto errLabel;
+          }
+
+          // if a dev_idx filter was given and the device does not match 
+          if( dev_idx != filter_dev_idx )
+            continue;
         }
 
         if(textIsEqual(type,"non"))
@@ -112,19 +274,20 @@ namespace cw
     rc_t run( perf_score::handle_t& scoreH,
               double          srate,
               unsigned        smp_per_cycle,
-              const char*     midi_csv_fname,
               const object_t* sf_cfg,
               unsigned        beg_loc_id,
               unsigned        end_loc_id,
+              bool            rpt_per_note_fl,
+              const midi_file_t& mf,
               score_follow_2::rpt_t& rpt_ref )
     {
 
       rc_t rc = kOkRC;
-      midi_file_t              mf{ .evtA = nullptr, .evtN=0, .sampleN=0 };
       score_follow_2::args_t   sf_args{};
       score_follow_2::handle_t sfH;
       unsigned                 midi_evt_idx = 0;
-
+      unsigned                 resultN = 0;
+      const score_follow_2::result_t* resultA = nullptr;
       // parse args
       if((rc = score_follow_2::parse_args(sf_cfg,sf_args)) != kOkRC )
       {
@@ -132,9 +295,6 @@ namespace cw
         goto errLabel;
       }
 
-      // parse MIDI file into an array
-      if((rc = parse_midi_file( midi_csv_fname, srate, mf )) != kOkRC )
-        goto errLabel;
       
       //cwLogInfo("Following: (%i notes found) sample count:%i %s.",mf.evtN,mf.sampleN,midi_csv_fname);
       
@@ -156,10 +316,10 @@ namespace cw
       {
         while( smp_idx <= mf.evtA[midi_evt_idx].sample_idx && mf.evtA[midi_evt_idx].sample_idx < smp_idx + smp_per_cycle )
         {
-          const midi_evt_t* e =  mf.evtA + midi_evt_idx++;
-          unsigned loc_id = kInvalidId;
-          unsigned score_vel = -1;
-          unsigned meas_numb = -1;
+          const midi_evt_t* e         = mf.evtA + midi_evt_idx++;
+          unsigned          loc_id    = kInvalidId;
+          unsigned          score_vel = -1;
+          unsigned          meas_numb = -1;
           
           //printf("%f pitch:%i vel:%i\n",e->sec,e->pitch,e->vel);
           
@@ -178,14 +338,148 @@ namespace cw
         
       }
 
+
+      if( rpt_per_note_fl)
+      {
+        if((resultA = result_array(sfH,resultN)) != nullptr )
+        {
+          cwLogInfo("uid,loc,pitch,vel");
+          for(unsigned i=0; i<resultN; ++i)
+          {
+            const auto* r = resultA + i;        
+            cwLogInfo("%i,%i,%i,%i",r->perf_uid,r->match_loc_id,r->perf_pitch,r->perf_vel);
+          }
+        }
+      }
       score_follow_2::report_summary(sfH,rpt_ref);
       
     errLabel:
-      mem::release(mf.evtA);
       destroy(sfH);
       
       return rc;
     }
+
+    rc_t _run_from_take_list(const object_t*        fileEle,
+                             perf_score::handle_t&  scoreH,
+                             double                 srate,
+                             unsigned               smp_per_cycle,
+                             const object_t*        sf_cfg,
+                             unsigned               beg_loc_id,
+                             unsigned               end_loc_id,
+                             unsigned               min_perf_noteN,
+                             unsigned               max_spuriousN,
+                             bool                   rpt_per_note_fl,
+                             score_follow_2::rpt_t& sf_rpt)
+    {
+      rc_t            rc       = kOkRC;
+      const object_t* takeL    = nullptr;
+      const char*     c_folder = nullptr;
+      char*           folder   = nullptr;
+      
+      if((rc = fileEle->getv("folder",c_folder,
+                             "takeL",takeL)) != kOkRC )
+      {
+        rc = cwLogError(rc,"Parse of 'folder' and 'takeL' of SF2 test case failed.");
+        goto errLabel;
+      }
+      
+      folder = filesys::expandPath( c_folder );
+
+      // for each take number
+      for(unsigned j=0; rc==kOkRC && j<takeL->child_count(); ++j)
+      {
+        unsigned take_num;
+
+        // read the take number
+        if((rc = takeL->child_ele(j)->value(take_num)) != kOkRC )
+        {
+          rc = cwLogError(rc,"Error parsing take number on test '%s' index '%i'.",cwStringNullGuard(c_folder),j);
+        }
+        else
+        {
+          midi_file_t mf{ .evtA = nullptr, .evtN=0, .sampleN=0 };
+          unsigned beg_perf_idx = kInvalidIdx;
+          unsigned end_perf_idx = kInvalidIdx;
+          unsigned dev_idx      = kInvalidIdx;
+          
+          // form the record_### folder name
+          char* record_folder = mem::printf<char>(nullptr,"record_%i",take_num);      
+          
+          // form MIDI csv file name
+          char* midi_csv_fname = filesys::makeFn(folder,"fix_midi","csv", record_folder, nullptr );
+
+          // parse MIDI file into an array
+          if((rc = parse_midi_file( midi_csv_fname, srate, kInvalidIdx, kInvalidIdx, kInvalidIdx, kCsvVersion0Id, mf )) == kOkRC )
+          {
+            // Run the SF against the MIDI file
+            if((rc = run(scoreH, srate, smp_per_cycle, sf_cfg, beg_loc_id, end_loc_id, rpt_per_note_fl, mf, sf_rpt )) != kOkRC )
+              rc = cwLogError(rc,"SF2 test run failed on '%s'.",cwStringNullGuard(midi_csv_fname));
+            else
+              cwLogInfo("Notes:%i Match:%i Miss:%i Spurious:%i : %s",sf_rpt.perfNoteN,sf_rpt.matchN,sf_rpt.missN,sf_rpt.spuriousN,cwStringNullGuard(midi_csv_fname));
+
+          }
+                    
+          mem::release(record_folder);
+          mem::release(midi_csv_fname);
+          mem::release(mf.evtA);
+          
+        }
+      }
+
+    errLabel:
+      mem::release(folder);
+      return rc;
+    }
+
+      rc_t _run_with_one_file(const object_t*        fileEle,
+                              perf_score::handle_t&  scoreH,
+                              double                 srate,
+                              unsigned               smp_per_cycle,
+                              const object_t*        sf_cfg,
+                              unsigned               beg_loc_id,
+                              unsigned               end_loc_id,
+                              unsigned               min_perf_noteN,
+                              unsigned               max_spuriousN,
+                              bool                   rpt_per_note_fl,
+                              score_follow_2::rpt_t& sf_rpt)
+      {
+        rc_t        rc;
+        unsigned    beg_perf_idx  = kInvalidIdx;
+        unsigned    end_perf_idx   = kInvalidIdx;
+        const char* perf_csv_fname = nullptr;
+        unsigned    dev_idx       = kInvalidIdx;
+        midi_file_t mf{ .evtA = nullptr, .evtN=0, .sampleN=0 };
+        
+        if((rc = fileEle->getv("beg_perf_idx",beg_perf_idx,
+                               "end_perf_idx",end_perf_idx,
+                               "file",perf_csv_fname,
+                               "dev_idx",dev_idx)) != kOkRC )
+        {
+          rc = cwLogError(rc,"Parse of 'file' in SF2 test case failed.");
+          goto errLabel;
+        }
+
+        // parse MIDI file into an array
+        if((rc = parse_midi_file( perf_csv_fname, srate, beg_perf_idx, end_perf_idx, dev_idx, kCsvVersion1Id, mf )) == kOkRC )
+        {
+          // Run the SF against the MIDI file
+          if((rc = run(scoreH, srate, smp_per_cycle, sf_cfg, beg_loc_id, end_loc_id, rpt_per_note_fl, mf, sf_rpt )) != kOkRC )
+            rc = cwLogError(rc,"SF2 test run failed on '%s'.",cwStringNullGuard(perf_csv_fname));
+          else
+          {
+            cwLogInfo("Notes:%i Match:%i Miss:%i Spurious:%i : %s",sf_rpt.perfNoteN,sf_rpt.matchN,sf_rpt.missN,sf_rpt.spuriousN,cwStringNullGuard(perf_csv_fname));
+          }
+
+        }
+
+      errLabel:
+
+        mem::release(mf.evtA);
+ 
+        return rc;
+        
+      }
+    
   }
 }
 
@@ -200,6 +494,7 @@ cw::rc_t cw::score_follow_2::test( const object_t* cfg )
   char*                score_fname   = nullptr;
   const object_t*      fileL         = nullptr;
   const object_t*      sf_cfg        = nullptr;
+  bool                 rpt_per_note_fl = false;
   
   unsigned             tot_missN     = 0;
   unsigned             tot_spuriousN = 0;
@@ -212,6 +507,7 @@ cw::rc_t cw::score_follow_2::test( const object_t* cfg )
                      "smp_per_cycle",smp_per_cycle,
                      "limit_perf_N",limit_perf_N,
                      "score_fname",c_score_fname,
+                     "rpt_per_note_fl",rpt_per_note_fl,
                      "fileL",fileL,
                      "follower",sf_cfg)) != kOkRC )
   {
@@ -235,73 +531,49 @@ cw::rc_t cw::score_follow_2::test( const object_t* cfg )
     unsigned        end_loc_id     = kInvalidId;
     unsigned        min_perf_noteN = 0;
     unsigned        max_spuriousN  = 0;
-    const char*     c_folder       = nullptr;
-    char*           folder         = nullptr;
+    
     const object_t* takeL          = nullptr;
     
     score_follow_2::rpt_t sf_rpt;
 
     // read the file record
     if((rc = fileL->child_ele(i)->getv("beg_loc",beg_loc_id,
-                                      "end_loc",end_loc_id,
+                                       "end_loc",end_loc_id,
                                        "min_perf_noteN",min_perf_noteN,
-                                       "max_spuriousN", max_spuriousN,
-                                      "folder",c_folder,
-                                      "takeL",takeL)) != kOkRC )
+                                       "max_spuriousN", max_spuriousN)) != kOkRC )
     {
-      rc = cwLogError(rc,"Parse of SF2 test case at index %i.",i);
+      rc = cwLogError(rc,"Parse of SF2 test case at index %i failed.",i);
       goto errLabel;
     }
 
-    folder = filesys::expandPath( c_folder );
-
-    // for each take number
-    for(unsigned j=0; j<takeL->child_count(); ++j)
+    if( fileL->child_ele(i)->find_child("folder") != nullptr )
     {
-      char* record_folder = nullptr;
-      unsigned take_num;
-
-      // read the take number
-      if((rc = takeL->child_ele(j)->value(take_num)) != kOkRC )
-      {
-        rc = cwLogError(rc,"Error parsing take number on test '%s' index '%i'.",cwStringNullGuard(c_folder),j);
-        break;
-      }
-
-      // form the record_### folder name
-      record_folder = mem::printf<char>(nullptr,"record_%i",take_num);      
-
-      // form MIDI csv file name
-      char* midi_csv_fname = filesys::makeFn(folder,"fix_midi","csv", record_folder, nullptr );
-
-      if((rc = run(scoreH, srate, smp_per_cycle, midi_csv_fname, sf_cfg, beg_loc_id, end_loc_id, sf_rpt )) != kOkRC )
-      {
-        rc = cwLogError(rc,"SF2 test run failed on '%s'.",cwStringNullGuard(midi_csv_fname));
-        break;
-      }
-
-      cwLogInfo("Notes:%i Match:%i Miss:%i Spurious:%i : %s",sf_rpt.perfNoteN,sf_rpt.matchN,sf_rpt.missN,sf_rpt.spuriousN,cwStringNullGuard(midi_csv_fname));
-
-      if( sf_rpt.perfNoteN > min_perf_noteN && sf_rpt.spuriousN < max_spuriousN )
-      {
-        tot_passN += 1;
-        tot_missN += sf_rpt.missN;
-        tot_spuriousN += sf_rpt.spuriousN;        
-      }
-      else
-      {
-        tot_failN += 1;
-      }
-      
-      //printf("%s\n",fname);
-
-      mem::release(record_folder);
-      mem::release(midi_csv_fname);
-      
+      rc = _run_from_take_list(fileL->child_ele(i),scoreH,srate,smp_per_cycle,sf_cfg,beg_loc_id,end_loc_id,min_perf_noteN,max_spuriousN,rpt_per_note_fl,sf_rpt);
+    }
+    else
+    {
+      rc = _run_with_one_file(fileL->child_ele(i),scoreH,srate,smp_per_cycle,sf_cfg,beg_loc_id,end_loc_id,min_perf_noteN,max_spuriousN,rpt_per_note_fl,sf_rpt);
     }
 
-    mem::release(folder);
+    if( rc != kOkRC )
+    {
+      rc = cwLogError(rc,"SF2 test case index %i failed.",i);
+      goto errLabel;
+    }
+    
+    
+    if( sf_rpt.perfNoteN > min_perf_noteN && sf_rpt.spuriousN < max_spuriousN )
+    {
+      tot_passN += 1;
+      tot_missN += sf_rpt.missN;
+      tot_spuriousN += sf_rpt.spuriousN;        
+    }
+    else
+    {
+      tot_failN += 1;
+    }
 
+      
     
   }
 
